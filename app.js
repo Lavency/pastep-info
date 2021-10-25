48328e7f926b9a21272f68c02f87afb56d8e56a5
Kermit hastam <65454734+Kermit-hastam@users.noreply.github.com>
2021-10-25 23:48:59 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const fetch = require("node-fetch");
+
+ async function id(username) {
+  let response = await fetch(`https://api.pastep.com/accounts/get?username=${username}`);
+  const data = await response.json();
+  return data.id
+}
+
+async function persianUsername(username) {
+  let response = await fetch(`https://api.pastep.com/accounts/get?username=${username}`);
+  const data = await response.json();
+  return data.persianUsername
+}
+
+async function bio(username) {
+  let response = await fetch(`https://api.pastep.com/accounts/get?username=${username}`);
+  const data = await response.json();
+  return data.bio
+}
+
+async function avatar(username) {
+  let response = await fetch(`https://api.pastep.com/accounts/get?username=${username}`);
+  const data = await response.json();
+  return data.avatar
+}
+
+async function id(username) {
+  let response = await fetch(`https://api.pastep.com/accounts/get?username=${username}`);
+  const data = await response.json();
+  return data.id
+}
+
+
+module.exports = { persianUsername, bio, avatar, id};
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Hello there , 👋
+
+I just developed this module just because I like the [**Pastep**](https://pastep.com) site !
+
+## Installing
+```
+npm i pastep-info
+```
+## Example of usage
+```js
+const pastep = require("pastep-info")
+
+
+pastep.id('username').then(data => console.log(data)) // For get user id
+pastep.persianUsername('username').then(data => console.log(data)) // For get user persian username
+pastep.avatar('username').then(data => console.log(data)) // For get user avatar url
+pastep.bio('username').then(data => console.log(data)) // For get user bio
+```
+## Support
+ - **[Discord Server](https://discord.gg/MHFTKe6g29)**
+ - **[Instagram]( https://www.instagram.com/kermit_hastam/)**
+ - **[GITHUB](https://github.com/Kermit-hastam)**
+
+****
+> ***Developed With ♥ by Kermit Xaro***
